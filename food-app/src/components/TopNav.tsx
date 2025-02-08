43864bd3a870cfa1185932740e826d749c262234
umutulay <umutulay1907@gmail.com>
2025-02-08 17:46:01 -0600

    seomthing

---
+++ food-app/src/components/TopNav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TopNav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+            Food App
+        </a>
+        </nav>
+    );
+};
+
+export default TopNav;
