d2d2b8422d6cc3dd22e3d230654b550c91ed0de3
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-02-08 21:10:00 -0600

    Updated webpage css

--- food-app/src/App.tsx
+++ food-app/src/App.tsx
@@ -7,6 +7,7 @@ import './App.css';
 
 //image imports
 import HappyPeopleWithFood from './images/happy-people-image.avif';
+import GroupOfPeople from './images/group-of-people.jpg'
 
 function App() {
   return (
@@ -21,8 +22,28 @@ function App() {
       </div>
 
       <div className="motto-container">
-          <p></p>
+          <p>Feeding Hope, Finding Help</p>
       </div>
+    </div>
+
+    <div className="row-2-container">
+
+      <div className="col-6" id="group-of-people-container">
+          <img src={GroupOfPeople} alt="Our Group" id="group-of-people-image"/>
+      </div>
+
+      <div className="col-6" id="mission-statement-container">
+          <p>Our mission is to bridge the gap between individuals in need and the resources that can make a 
+            difference in their lives. Guided by compassion and a commitment to fostering equity, we strive 
+            to connect people to local food banks, shelters, and aid programs, ensuring that no one goes 
+            hungry. By leveraging technology, community partnerships, and innovative solutions, we empower 
+            families and individuals to access nutritious meals and vital support. Together, we envision a 
+            future where hunger is no longer a barrier to opportunity, and every person has the chance to 
+            thrive with dignity and hope. Through our work, we aim to inspire meaningful connections, create 
+            a ripple effect of generosity, and build stronger, hunger-free communities for 
+            generations to come.</p>
+      </div>
+
     </div>
     </>
   );
