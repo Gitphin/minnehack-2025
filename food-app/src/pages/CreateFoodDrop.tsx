2721418fc06fad6c1c5c32047483acd7f2f26d85
Alek H <alekh@debian.dolphin>
2025-02-09 09:52:52 -0600

    final changes before coffman

--- food-app/src/pages/CreateFoodDrop.tsx
+++ food-app/src/pages/CreateFoodDrop.tsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import TopNav from "../components/TopNav";
 import axios from "axios";
 import "react-datepicker/dist/react-datepicker.css";
+import "./createpage.css"; // Import CSS
 
-const CreatePage: React.FC = () => {
+const CreateFoodDrop: React.FC = () => {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [foodType, setFoodType] = useState("");
@@ -28,16 +29,34 @@ const CreatePage: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="create-page-container">
       <TopNav />
       <h2>Create a Food Drop</h2>
-      <input placeholder="Restaurant Name" value={name} onChange={(e) => setName(e.target.value)} />
-      <input placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} />
-      <input placeholder="Food Type" value={foodType} onChange={(e) => setFoodType(e.target.value)} />
-      <input type="number" placeholder="Max Claims" value={maxClaims} onChange={(e) => setMaxClaims(Number(e.target.value))} />
+      <input
+        placeholder="Restaurant Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        placeholder="Address"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+      />
+      <input
+        placeholder="Food Type"
+        value={foodType}
+        onChange={(e) => setFoodType(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Max Claims"
+        value={maxClaims}
+        onChange={(e) => setMaxClaims(Number(e.target.value))}
+      />
       <button onClick={handleCreate}>Create</button>
     </div>
   );
 };
 
-export default CreatePage;
+export default CreateFoodDrop;
+
