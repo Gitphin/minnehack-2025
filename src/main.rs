943f939d961ab2d0fc72716b9c9242fcf103e06b
Arthur Beck <averse.abfun@gmail.com>
2025-02-09 09:24:45 -0600

    Last commit on together before I merge main and together

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,22 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
+#![feature(unboxed_closures)]
+
 use actix_web::web::Data;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use actix_web::HttpServer;
 use actix_web::App;
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
 use crate::food_bank::find_food_shelters_route;
 use crate::restaurants::find_restaurants_route;
 use actix_cors::Cors;
 
-mod format;
+mod storage;
 mod food_bank;
 mod restaurants;
 mod helper_funcs;
 mod gcloud;
+mod events;
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Is this working?"))))
-}
-
-#[tokio::main]
+#[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let client = gcloud::init()?;
     
@@ -38,36 +28,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             .wrap(Cors::permissive()) // Allow all origins for development
             .service(find_restaurants_route)
             .service(find_food_shelters_route)
+            .service(events::event_routes())
     })
     .bind("0.0.0.0:3001")? // Use a different port for Actix
     .run();
 
-    let hyper_server = async {
-        let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-        let listener = TcpListener::bind(addr).await?;
-    
-        loop {
-            let (stream, _) = listener.accept().await?;
-            let io = TokioIo::new(stream);
-    
-            tokio::task::spawn(async move {
-                if let Err(err) = http1::Builder::new()
-                    .serve_connection(io, service_fn(hello))
-                    .await
-                {
-                    eprintln!("Error serving connection: {:?}", err);
-                }
-            });
-        }
-
-        #[allow(unreachable_code)]
-        Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
-    };
-
-    tokio::select! {
-        _ = http_server => (),
-        _ = hyper_server => (),
-    }
+    http_server.await?;
 
     Ok(())
 }
