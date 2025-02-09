0353fe25ed2ff966b89d39e1966573e62e2ab604
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-02-08 19:22:35 -0600

    Updated css

--- food-app/src/App.tsx
+++ food-app/src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 //component imports
 import TopNav from "./components/TopNav";
 import FoodGroups from "./components/FoodGroups"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";  // Ensure JS is loaded
 
 //image imports
 import HappyPeopleWithFood from './images/happy-people-with-food.jpg';
@@ -9,7 +11,7 @@ import HappyPeopleWithFood from './images/happy-people-with-food.jpg';
 function App() {
   return (
     <>
-    <div>
+    <div className='top-nav-container'>
       <TopNav />
     </div>
 
